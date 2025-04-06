f1b15b723686b51f9dc9aa6413e1f269775cb4ae
RaulCD3597 <raulcd3597@gmail.com>
2025-04-06 10:10:29 -0400

    update project

--- main.py
+++ main.py
@@ -1,47 +1,41 @@
-from typing import Dict, List
+import sys
+
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
 
 
 def main():
-    book_path = "books/frankenstein.txt"
-    with open(book_path) as file:
-        file_contents = file.read()
-        print_report(book_path, file_contents)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
-def count_words(text: str) -> int:
-    return len(text.split())
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 
-def character_count(text: str) -> Dict[str, int]:
-    characters = {}
-    for c in text:
-        character = c.lower()
-        if character in characters:
-            characters[character] += 1
-        else:
-            characters[character] = 1
-    return characters
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
 
-
-def dict_to_list(char_dict: Dict[str, int]) -> List[dict]:
-    return [
-        {"char": key, "count": value}
-        for key, value in char_dict.items()
-        if key.isalpha()
-    ]
-
-
-def print_report(book_path: str, book_content: str):
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{count_words(book_content)} words found in the document")
-    print()
-    char_list = dict_to_list(character_count(book_content))
-    char_list.sort(reverse=True, key=lambda dict: dict["count"])
-    for char in char_list:
-        character = char["char"]
-        amount = char["count"]
-        print(f"The '{character}' character was found {amount} times")
-    print("--- End report ---")
+    print("============= END ===============")
 
 
 main()
